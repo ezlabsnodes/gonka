3c126f94ae5ae5745c32551816daf70b756fd20a
Rayan Shifa <ezlabsnodes@gmail.com>
2026-02-06 10:02:42 +0300

    Update register-disable-mlnode.sh

--- register-disable-mlnode.sh
+++ register-disable-mlnode.sh
@@ -48,12 +48,12 @@ if [ "$MAIN_CHOICE" == "1" ]; then
   "host": "$GPU_HOST",
   "inference_port": 5050,
   "poc_port": 8080,
-  "max_concurrent": 200,
+  "max_concurrent": 400,
 
   "models": {
     "Qwen/Qwen3-235B-A22B-Instruct-2507-FP8": {
       "args": [
-        "--tensor-parallel-size", "4"
+        "--tensor-parallel-size", "8"
       ]
     }
   }
